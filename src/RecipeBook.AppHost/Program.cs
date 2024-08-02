0df5ddc3991d2bf978829b6ae3901f336e8fcc8b
Andres Hernandez <andhernand@gmail.com>
2024-08-02 17:26:16 -0600

    add mongodb to the Aspire host
    It also wires up the API to use the database.

--- src/RecipeBook.AppHost/Program.cs
+++ src/RecipeBook.AppHost/Program.cs
@@ -1,10 +1,26 @@
+using DotNetEnv;
+
+using static RecipeBook.AppHost.Utilities;
+
+Env.TraversePath().Load();
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("cache");
+var db = builder.AddMongoDB("DefaultMongoDb")
+    .WithImage("mongodb/mongodb-community-server", "7.0.12-ubuntu2204")
+    .WithEnvironment("RECIPE_BOOK_API_USER", GetEnvironmentVariableOrThrow("RECIPE_BOOK_API_USER"))
+    .WithEnvironment("RECIPE_BOOK_API_PWD", GetEnvironmentVariableOrThrow("RECIPE_BOOK_API_PWD"))
+    .WithEnvironment("RECIPE_BOOK_API_DATABASE", GetEnvironmentVariableOrThrow("RECIPE_BOOK_API_DATABASE"))
+    .WithEnvironment("RECIPE_BOOK_API_COLLECTION", GetEnvironmentVariableOrThrow("RECIPE_BOOK_API_COLLECTION"))
+    .WithBindMount("../../db/mongo-init.sh", "/docker-entrypoint-initdb.d/mongo-init.sh", true)
+    .WithMongoExpress(x => x.WithImage("mongo-express", "1.0.2-20-alpine3.19"));
 
-var apiService = builder.AddProject<Projects.RecipeBook_ApiService>("apiservice");
+var cache = builder.AddRedis("cache").WithImage("redis", "7.4");
 
-builder.AddProject<Projects.RecipeBook_Web>("webfrontend")
+var apiService = builder.AddProject<Projects.RecipeBook_ApiService>("recipe-book-api")
+    .WithReference(db);
+
+builder.AddProject<Projects.RecipeBook_Web>("recipe-book-web")
     .WithExternalHttpEndpoints()
     .WithReference(cache)
     .WithReference(apiService);
---
+++ src/RecipeBook.AppHost/Utilities.cs
@@ -0,0 +1,11 @@
+﻿namespace RecipeBook.AppHost
+{
+    internal static class Utilities
+    {
+        internal static string GetEnvironmentVariableOrThrow(string variableName)
+        {
+            return Environment.GetEnvironmentVariable(variableName) ??
+                   throw new InvalidOperationException($"Environment variable not set: {variableName}");
+        }
+    }
+}
