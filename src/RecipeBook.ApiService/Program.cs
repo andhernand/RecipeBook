181b54817bf7a4e52b42bf7c68a9d9e3acc97915
Andres Hernandez <andhernand@gmail.com>
2024-08-03 01:06:49 -0600

    Cleanup service application  configuration

--- src/RecipeBook.ApiService/Program.cs
+++ src/RecipeBook.ApiService/Program.cs
@@ -1,55 +1,25 @@
-using System.Diagnostics;
-
-using MongoDB.Driver;
-
-using RecipeBook.ApiService.Options;
+using RecipeBook.ApiService;
 using RecipeBook.ServiceDefaults;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.AddServiceDefaults();
-
-builder.Services.AddProblemDetails(x =>
 {
-    x.CustomizeProblemDetails = context =>
-    {
-        if (!context.ProblemDetails.Extensions.ContainsKey("traceId"))
-        {
-            context.ProblemDetails.Extensions["traceId"] =
-                Activity.Current?.Id ?? context.HttpContext.TraceIdentifier;
-        }
-    };
-});
+    builder.AddServiceDefaults();
+    builder.AddRecipeBookDefaults();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddOptions<RecipeBookDatabaseOptions>()
-    .BindConfiguration(RecipeBookDatabaseOptions.Key)
-    .ValidateDataAnnotations()
-    .ValidateOnStart();
-
-builder.AddMongoDBClient("DefaultMongoDb", configureClientSettings: x =>
-{
-    var dbOptions = new RecipeBookDatabaseOptions();
-    builder.Configuration.GetSection(RecipeBookDatabaseOptions.Key).Bind(dbOptions);
-
-    x.ApplicationName = "recipe-book-api";
-    x.Credential = MongoCredential.CreateCredential(
-        dbOptions.DatabaseName,
-        dbOptions.Username,
-        dbOptions.Password);
-});
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 }
 
-app.UseExceptionHandler();
-app.MapDefaultEndpoints();
+var app = builder.Build();
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseGlobalErrorHandling();
+    app.MapDefaultEndpoints();
+}
 
 app.Run();
