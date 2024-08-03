576c739ee5a3d07247ed5aa36da988771bc4e244
Andres Hernandez <andhernand@gmail.com>
2024-08-03 01:52:17 -0600

    Add Validators and Mappings

---
+++ src/RecipeBook.ApiService/Mapping/ContractMapping.cs
@@ -0,0 +1,47 @@
+﻿using RecipeBook.ApiService.Models;
+using RecipeBook.Contracts.Requests;
+using RecipeBook.Contracts.Responses;
+
+namespace RecipeBook.ApiService.Mapping;
+
+public static class ContractMapping
+{
+    public static Recipe MapToRecipe(this CreateRecipeRequest request)
+    {
+        return new Recipe
+        {
+            Title = request.Title,
+            Description = request.Description,
+            Ingredients = request.Ingredients,
+            Directions = request.Directions
+        };
+    }
+
+    public static Recipe MapToRecipe(this UpdateRecipeRequest request)
+    {
+        return new Recipe
+        {
+            Title = request.Title,
+            Description = request.Description,
+            Ingredients = request.Ingredients,
+            Directions = request.Directions
+        };
+    }
+
+    public static RecipeResponse MapToResponse(this Recipe recipe)
+    {
+        return new RecipeResponse
+        {
+            Id = recipe.Id,
+            Title = recipe.Title,
+            Description = recipe.Description,
+            Ingredients = recipe.Ingredients,
+            Directions = recipe.Directions
+        };
+    }
+
+    public static RecipesResponse MapToResponse(this IEnumerable<Recipe> recipes)
+    {
+        return new RecipesResponse { Recipes = recipes.Select(MapToResponse) };
+    }
+}
---
+++ src/RecipeBook.ApiService/Regex/RegexPatterns.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RecipeBook.ApiService.Regex;
+
+public static partial class RegexPatterns
+{
+    [GeneratedRegex("^[0-9a-fA-F]{24}$", RegexOptions.Compiled, 5)]
+    public static partial System.Text.RegularExpressions.Regex HexIdRegex();
+}
