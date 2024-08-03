7682c79fc6eacfe26d4900afb4a1da2297e54d2a
Andres Hernandez <andhernand@gmail.com>
2024-08-03 01:52:17 -0600

    Add Validators and Mappings

---
+++ src/RecipeBook.ApiService/Validators/UpdateRecipeRequestValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+
+using RecipeBook.Contracts.Requests;
+
+namespace RecipeBook.ApiService.Validators;
+
+public class UpdateRecipeRequestValidator : AbstractValidator<UpdateRecipeRequest>
+{
+    public UpdateRecipeRequestValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty();
+
+        RuleFor(x => x.Description)
+            .NotEmpty();
+
+        RuleFor(x => x.Ingredients)
+            .NotEmpty();
+
+        RuleForEach(x => x.Ingredients)
+            .NotEmpty()
+            .WithMessage("Ingredient at index {CollectionIndex} must not be empty.");
+
+        RuleFor(x => x.Directions)
+            .NotEmpty();
+
+        RuleForEach(x => x.Directions)
+            .NotEmpty()
+            .WithMessage("Direction at index {CollectionIndex} must not be empty.");
+    }
+}
