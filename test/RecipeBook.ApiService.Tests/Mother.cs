c882facb6c8b49561a82f59bcc4180969937aa57
Andres Hernandez <andhernand@gmail.com>
2024-08-04 19:35:17 -0600

    Adding the first test for the API.
    More tests need to be added. But, I did figure out how to test the distributed application in a way that I am comfortable with.
    
    Some cleanup and removal of unnecessary references needs to happen. but this is the initial part of the process.

---
+++ test/RecipeBook.ApiService.Tests/Mother.cs
@@ -0,0 +1,75 @@
+﻿using System.Net.Http.Json;
+
+using Bogus;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using RecipeBook.Contracts.Requests;
+using RecipeBook.Contracts.Responses;
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace RecipeBook.ApiService.Tests;
+
+public static class Mother
+{
+    public const string RecipesApiBasePath = "/api/recipes";
+
+    public static CreateRecipeRequest GenerateCreateRecipeRequest(
+        string? title = default,
+        string? description = default,
+        IEnumerable<string>? ingredients = default,
+        IEnumerable<string>? directions = default)
+    {
+        var recipeFaker = new Faker<CreateRecipeRequest>()
+            .RuleFor(x => x.Title, f => title ?? f.Lorem.Sentence(3))
+            .RuleFor(x => x.Description, f => description ?? f.Lorem.Sentence(10))
+            .RuleFor(x => x.Ingredients, f => ingredients ?? f.Make(6, () => f.Lorem.Sentence(3)))
+            .RuleFor(x => x.Directions, f => directions ?? f.Make(10, () => f.Lorem.Sentence(10)));
+
+        return recipeFaker.Generate();
+    }
+
+    public static UpdateRecipeRequest GenerateUpdateRecipeRequest(
+        string? title = default,
+        string? description = default,
+        IEnumerable<string>? ingredients = default,
+        IEnumerable<string>? directions = default)
+    {
+        var recipeFaker = new Faker<UpdateRecipeRequest>()
+            .RuleFor(x => x.Title, f => title ?? f.Lorem.Sentence(3))
+            .RuleFor(x => x.Description, f => description ?? f.Lorem.Sentence(10))
+            .RuleFor(x => x.Ingredients, f => ingredients ?? f.Make(6, () => f.Lorem.Sentence(3)))
+            .RuleFor(x => x.Directions, f => directions ?? f.Make(10, () => f.Lorem.Sentence(10)));
+
+        return recipeFaker.Generate();
+    }
+
+    public static ProblemDetails GenerateNotFoundProblemDetails()
+    {
+        return new ProblemDetails
+        {
+            Type = "https://tools.ietf.org/html/rfc9110#section-15.5.5",
+            Title = "Not Found",
+            Status = StatusCodes.Status404NotFound
+        };
+    }
+
+    public static ValidationProblemDetails GenerateValidationProblemDetails(Dictionary<string, string[]> errors)
+    {
+        return new ValidationProblemDetails(errors)
+        {
+            Type = "https://tools.ietf.org/html/rfc9110#section-15.5.1",
+            Title = "One or more validation errors occurred.",
+            Status = StatusCodes.Status400BadRequest
+        };
+    }
+
+    public static async Task<RecipeResponse> CreateRecipeAsync(HttpClient client)
+    {
+        var request = GenerateCreateRecipeRequest();
+        var response = await client.PostAsJsonAsync(RecipesApiBasePath, request);
+        var recipe = await response.Content.ReadFromJsonAsync<RecipeResponse>();
+        return recipe!;
+    }
+}
---
+++ test/RecipeBook.ApiService.Tests/RecipeBookApiTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http.Json;
+
+using FluentAssertions;
+
+using RecipeBook.Contracts.Responses;
+
+namespace RecipeBook.ApiService.Tests;
+
+public class RecipeBookApiTests : IClassFixture<RecipeBookApiFactory>
+{
+    private readonly RecipeBookApiFactory _factory;
+
+    // ReSharper disable once ConvertToPrimaryConstructor
+    public RecipeBookApiTests(RecipeBookApiFactory factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task GetAllRecipes_WhenNoRecipesExist_ShouldReturnNoRecipes()
+    {
+        // Arrange
+        using var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync(Mother.RecipesApiBasePath);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var recipes = await response.Content.ReadFromJsonAsync<RecipesResponse>();
+        recipes!.Recipes.Should().BeEmpty();
+    }
+}
