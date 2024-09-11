f5ce1510a710e1e27e5966e0989eb530664cf162
Andres Hernandez <andhernand@gmail.com>
2024-09-11 11:28:20 -0600

    refactor Program.cs
    add additional test for update recipe endpoint.
    
    update deps.

--- src/RecipeBook.ApiService/Program.cs
+++ src/RecipeBook.ApiService/Program.cs
@@ -5,83 +5,59 @@ using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.OpenTelemetry;
 
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Information()
-    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
-    .WriteTo.Console(outputTemplate:
-        "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Properties} {Message:lj}{NewLine}{Exception}")
-    .Enrich.FromLogContext()
-    .Enrich.WithEnvironmentName()
-    .Enrich.WithProcessId()
-    .Enrich.WithThreadId()
-    .Enrich.WithMachineName()
-    .Enrich.WithProperty("Application", "Recipe Book API")
-    .CreateBootstrapLogger();
-
-try
+var builder = WebApplication.CreateBuilder(args);
 {
-    Log.Information("Starting Recipe Book API");
-
-    var builder = WebApplication.CreateBuilder(args);
-    {
-        builder.Host.UseSerilog((context, configuration) => configuration
-            .MinimumLevel.Information()
-            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
-            .WriteTo.Console(outputTemplate:
-                "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Properties} {Message:lj}{NewLine}{Exception}")
-            .WriteTo.OpenTelemetry(x =>
-            {
-                var otlpEndpoint = context.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
-
-                if (!string.IsNullOrWhiteSpace(otlpEndpoint))
-                {
-                    x.Endpoint = otlpEndpoint;
-
-                    var otelHeader = context.Configuration["OTEL_EXPORTER_OTLP_HEADERS"]!.Split('=');
-                    x.Headers = new Dictionary<string, string> { { otelHeader[0], otelHeader[1] } };
-
-                    x.Protocol = context.Configuration["OTEL_EXPORTER_OTLP_PROTOCOL"] == "grpc"
-                        ? OtlpProtocol.Grpc
-                        : OtlpProtocol.HttpProtobuf;
-                }
-            })
-            .Enrich.FromLogContext()
-            .Enrich.WithEnvironmentName()
-            .Enrich.WithProcessId()
-            .Enrich.WithThreadId()
-            .Enrich.WithMachineName()
-            .Enrich.WithProperty("Application", "Recipe Book API")
-        );
-
-        builder.AddServiceDefaults();
-
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        builder.AddRecipeBookDefaults();
-    }
-
-    var app = builder.Build();
-    {
-        if (app.Environment.IsDevelopment())
+    builder.Host.UseSerilog((context, configuration) => configuration
+        .MinimumLevel.Information()
+        .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+        .WriteTo.Console(outputTemplate:
+            "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Properties} {Message:lj}{NewLine}{Exception}")
+        .WriteTo.OpenTelemetry(x =>
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+            var otlpEndpoint = context.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
 
-        app.MapDefaultEndpoints();
-        app.UseSerilogRequestLogging();
-        app.UseGlobalErrorHandling();
-        app.MapApiEndpoints();
+            if (string.IsNullOrWhiteSpace(otlpEndpoint))
+            {
+                return;
+            }
+
+            x.Endpoint = otlpEndpoint;
+
+            var otelHeader = context.Configuration["OTEL_EXPORTER_OTLP_HEADERS"]!.Split('=');
+            x.Headers = new Dictionary<string, string> { { otelHeader[0], otelHeader[1] } };
+
+            x.Protocol = context.Configuration["OTEL_EXPORTER_OTLP_PROTOCOL"] == "grpc"
+                ? OtlpProtocol.Grpc
+                : OtlpProtocol.HttpProtobuf;
+        })
+        .Enrich.FromLogContext()
+        .Enrich.WithEnvironmentName()
+        .Enrich.WithProcessId()
+        .Enrich.WithThreadId()
+        .Enrich.WithMachineName()
+        .Enrich.WithProperty("Application", "Recipe Book API")
+    );
+
+    builder.AddServiceDefaults();
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.AddRecipeBookDefaults();
+}
+
+var app = builder.Build();
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
     }
 
-    app.Run();
+    app.MapDefaultEndpoints();
+    app.UseSerilogRequestLogging();
+    app.UseGlobalErrorHandling();
+    app.MapApiEndpoints();
 }
-catch (Exception ex)
-{
-    Log.Fatal(ex, "Application terminated unexpectedly");
-}
-finally
-{
-    Log.CloseAndFlush();
-}
+
+app.Run();
